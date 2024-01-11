c0db99b7ab60aeb6df2045d23267cea14ae4d2a6
Gravizz <peeke0rr@gmail.com>
2024-01-11 16:06:40 +0700

    feat:add answer ex5

--- exercise-5.js
+++ exercise-5.js
@@ -1,1 +1,9 @@
 // Paste the complete MQL here
+db.pizzaOrders.aggregate([
+  {
+    $group: {
+      _id: { month: { $month: "$created_at" }, year: { $year: "$created_at" } },
+      total_sale: { $sum: "$total_price" },
+    },
+  },
+]);
